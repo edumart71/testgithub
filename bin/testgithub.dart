2403706ec9bf96ea7fe9998385134c18e7294e3c
edumart71 <roberto.cardiel@gmail.com>
2021-10-06 21:55:26 -0500

    Revert "feat:[FUNC][JIRA]-added suma func"
    This reverts commit f09ca40e698d7ee618279decb23290e3ea148f21.

--- bin/testgithub.dart
+++ bin/testgithub.dart
@@ -3,5 +3,3 @@ import 'package:testgithub/testgithub.dart' as testgithub;
 void main(List<String> arguments) {
   print('Hello world: ${testgithub.calculate()}!');
 }
-
-int suma(int i, int y) => i + y;
