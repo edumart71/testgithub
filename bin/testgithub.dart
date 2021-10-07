f09ca40e698d7ee618279decb23290e3ea148f21
edumart71 <roberto.cardiel@gmail.com>
2021-10-06 21:54:13 -0500

    feat:[FUNC][JIRA]-added suma func

--- bin/testgithub.dart
+++ bin/testgithub.dart
@@ -3,3 +3,5 @@ import 'package:testgithub/testgithub.dart' as testgithub;
 void main(List<String> arguments) {
   print('Hello world: ${testgithub.calculate()}!');
 }
+
+int suma(int i, int y) => i + y;
